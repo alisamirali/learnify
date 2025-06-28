5489df9b7c41a0918c537dd731bc7f2594169e5a
Ali Samir <alisamir.eng@gmail.com>
2025-06-28 17:21:17 +0300

    feat: integrate Arcjet for enhanced signup protection and add HomePage component

---
+++ lib/arcjet.ts
@@ -0,0 +1,28 @@
+import { env } from "@/lib/env";
+import arcjet, {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+} from "@arcjet/next";
+
+export {
+  detectBot,
+  fixedWindow,
+  protectSignup,
+  sensitiveInfo,
+  shield,
+  slidingWindow,
+};
+
+export default arcjet({
+  key: env.ARCJET_KEY,
+  characteristics: ["fingerprint"],
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+  ],
+});
