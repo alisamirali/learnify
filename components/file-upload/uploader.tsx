e7e020c25b5756dcda4079f8ef99ea690e47e230
Ali Samir <ali.samir@taager.com>
2025-07-02 16:15:25 +0300

    feat: implement file deletion functionality and enhance uploader component

--- components/file-upload/uploader.tsx
+++ components/file-upload/uploader.tsx
@@ -2,7 +2,7 @@
 
 import { Card, CardContent } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FileRejection, useDropzone } from "react-dropzone";
 import { toast } from "sonner";
 import { v4 as uuidv4 } from "uuid";
@@ -25,7 +25,12 @@ type UploaderState = {
   fileType: "image" | "video";
 };
 
-export function Uploader() {
+type UploaderProps = {
+  value?: string;
+  onChange?: (value: string) => void;
+};
+
+export function Uploader({ value, onChange }: UploaderProps) {
   const [fileState, setFileState] = useState<UploaderState>({
     error: false,
     file: null,
@@ -34,6 +39,7 @@ export function Uploader() {
     progress: 0,
     isDeleting: false,
     fileType: "image",
+    key: value,
   });
 
   async function uploadFile(file: File) {
@@ -95,6 +101,8 @@ export function Uploader() {
               key: key,
             }));
 
+            onChange?.(key);
+
             toast.success("File uploaded successfully!");
 
             resolve();
@@ -149,6 +157,64 @@ export function Uploader() {
     [fileState.objectUrl]
   );
 
+  async function handleRemoveFile() {
+    if (fileState.isDeleting || !fileState.objectUrl) return;
+
+    try {
+      setFileState((prev) => ({
+        ...prev,
+        isDeleting: true,
+      }));
+
+      const response = await fetch("/api/s3/delete", {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ key: fileState.key }),
+      });
+
+      if (!response.ok) {
+        toast.error("Failed to delete file. Please try again.");
+
+        setFileState((prev) => ({
+          ...prev,
+          isDeleting: false,
+          error: true,
+        }));
+
+        return;
+      }
+
+      if (fileState.objectUrl && !fileState.objectUrl.startsWith("http")) {
+        URL.revokeObjectURL(fileState.objectUrl);
+      }
+
+      onChange?.("");
+
+      setFileState(() => ({
+        file: null,
+        uploading: false,
+        progress: 0,
+        objectUrl: undefined,
+        error: false,
+        id: null,
+        fileType: "image",
+        isDeleting: false,
+      }));
+
+      toast.success("File deleted successfully!");
+    } catch (error) {
+      toast.error("Failed to delete file. Please try again.");
+
+      setFileState((prev) => ({
+        ...prev,
+        isDeleting: false,
+        error: true,
+      }));
+    }
+  }
+
   function rejectedFiles(rejectedFiles: FileRejection[]) {
     if (rejectedFiles.length) {
       const tooManyFiles = rejectedFiles.find(
@@ -184,11 +250,25 @@ export function Uploader() {
     }
 
     if (fileState.objectUrl) {
-      return <RenderUploadedState previewUrl={fileState.objectUrl} />;
+      return (
+        <RenderUploadedState
+          previewUrl={fileState.objectUrl}
+          isDeleting={fileState.isDeleting}
+          handleRemoveFile={handleRemoveFile}
+        />
+      );
     }
 
     return <RenderEmptyState isDragActive={false} />;
   }
+
+  useEffect(() => {
+    return () => {
+      if (fileState.objectUrl && !fileState.objectUrl.startsWith("http")) {
+        URL.revokeObjectURL(fileState.objectUrl);
+      }
+    };
+  }, [fileState.objectUrl]);
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
@@ -199,6 +279,7 @@ export function Uploader() {
     multiple: false,
     maxSize: 5 * 1024 * 1024, // 5 MB
     onDropRejected: rejectedFiles,
+    disabled: fileState.uploading || !!fileState.objectUrl,
   });
 
   return (
